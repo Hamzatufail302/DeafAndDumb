4a174cf52e83e5e8eb3f72f31f1aa7a03913b69d
Hamza Tufail <hamzatufail217@gmail.com>
2019-12-04 11:13:41 -0800

    names

--- app/src/main/java/com/example/elearningfordeafanddumbkids/login.java
+++ app/src/main/java/com/example/elearningfordeafanddumbkids/login.java
@@ -1,14 +1,73 @@
 package com.example.elearningfordeafanddumbkids;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
+    private TextView textView1;
+    private FirebaseAuth firebaseAuth;
+    private Button button;
+    private EditText name,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        textView1 = (TextView) findViewById(R.id.textView);
+        firebaseAuth=firebaseAuth.getInstance();
+        button=(Button)findViewById(R.id.button21);
+        name=(EditText)findViewById(R.id.lname);
+        password=(EditText)findViewById(R.id.lpass);
+
+        FirebaseAuth user=FirebaseAuth.getInstance();
+        if(user!=null){
+            finish();
+            startActivity(new Intent(login.this,choose_profile.class));
+        }
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(login.this, Registration.class));
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(name.getText().toString(),password.getText().toString());
+            }
+
+        });
+
+
+
+    }
+
+    private void validate(String username, String userpass) {
+        firebaseAuth.createUserWithEmailAndPassword(username,userpass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(login.this,"login Success",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(login.this,choose_profile.class));
+                }
+                else{
+                    Toast.makeText(login.this,"login failed",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
